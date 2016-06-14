b2d09be04d6df50459f553a0f8055a83873aff62
ameetkall <ameetk96@gmail.com>
2016-06-14 15:36:43 -0500

    Pushing Majority xml files

---
+++ app/src/main/java/com/example/android6/movieproject/SettingsActivity.java
@@ -0,0 +1,52 @@
+package com.example.android6.movieproject;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+    Button clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        clear = (Button)findViewById(R.id.bActClearButton);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(SettingsActivity.this)
+                        .setTitle("Clear Favorites")
+                        .setMessage("Are you sure you want to clear all favorites?")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                SharedPreferences.Editor editor = getSharedPreferences("MovieFaves", Context.MODE_PRIVATE).edit();
+                                editor.clear();
+                                editor.commit();
+                                startActivity(new Intent(SettingsActivity.this,MainActivity.class));
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
+
+
+    }
+
+}
